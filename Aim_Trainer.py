d031f4bbf26c10efdaeca541abecc48d3e68d006
10024564 <10024564@american.fmtusd.org>
2022-03-22 14:36:56 -0700

    Created variables, Started menu

--- Aim_Trainer.py
+++ Aim_Trainer.py
@@ -1,3 +1,31 @@
 import pygame
+import random
+import sys
+import os
+import math
+from pygame.locals import *
 
-#2D KovaaK's Aim Trainer
+pygame.init()
+
+#Initialize Canvas/Window
+CanvasW = 1000
+CanvasH = 1000
+
+#Initialize Colors
+Black = (0,0,0)
+Red = (255,0,0)
+Blue = (0,0,255)
+White = (255,255,255)
+Green = (0,255,0)
+
+#Initialize Font
+Font = pygame.font.SysFont("Bangers", 50)
+
+#Initialize Images
+Aim_Target = pygame.transform.scale(Images/"Target.png", (50,50))
+Play_Button = pygame.transform.scale(Images/"Play.png", (100,100))
+Play_Again_Button = pygame.transform.scale(Images/"Play_Again.jpg", (100,100))
+
+def main_menu():
+    while True:
+        screen.fill(Black)
