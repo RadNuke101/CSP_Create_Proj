6004748ebb06ba41cec39f837e305ed91c10585e
10024564 <10024564@american.fmtusd.org>
2022-03-29 15:11:08 -0700

    working on main menu

--- Aim_Trainer.py
+++ Aim_Trainer.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import pygame
 import random
 import sys
@@ -20,7 +21,7 @@ White = (255,255,255)
 Green = (0,255,0)
 
 #Initialize Font
-Font = pygame.font.SysFont("Bangers", 50)
+Font = pygame.font.SysFont("Retro.ttf", 50)
 
 #Initialize Caption
 pygame.display.set_caption("Aim Trainer")
@@ -40,37 +41,37 @@ Play_Again_Button = pygame.transform.scale(B, (100,100))
 #Initialize Time
 Clock = pygame.time.Clock()
 
+#Loading Screen
+def intro():
+    pass
+
 #Main Menu - Can select difficulty, exit, pause
 def main_menu():
+    selected = "Start"
     color1 = White
     color2 = Red
     time = 0
     while True:
         screen.fill(Black)
-        difficulty = [] #Create a list
-        difficulty.append(pygame.Rect(5,450,250,125))
-        difficulty.append(pygame.Rect(255,450,250,125))
-        difficulty.append(pygame.Rect(505,450,250,125))
         for event in pygame.event.get():
             if event.type == quit:
+                pygame.quit()
                 quit()
             elif event.type == pygame.KEY_DOWN:
-                if event.key == pygame.K_ESCAPE:
+                if event.key == pygame.K_UP:
+                    selected = "Start"
+                elif event.key == pygame.K_ESCAPE:
                     main_menu()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if difficulty[0].collidepoint(pygame.mouse.get_pos()):
-                    game("Easy")
-                if difficulty[1].collidepoint(pygame.mouse.get_pos()):
-                    game("Medium")
-                if difficulty[2].collidepoint(pygame.mouse.get_pos()):
-                    game("Hard")
-        for i in difficulty:
-            pygame.draw.rect(screen, Red, pygame.rect)
-        Txt("Aim Trainer", screen, 90, 150, pygame.font.SysFont("Bangers", 112), color1)
-        Txt("Choose a Difficulty", screen, 90, 300, pygame.font.SysFont("Bangers", 100), color2)
-        Txt("Easy", screen, 85, 485, Font, Black)
-        Txt("Medium", screen, 315, 485, Font, Black)
-        Txt("Hard", screen, 580, 485, Font, Black)
+                elif event.key == pygame.K_DOWN:
+                    selected = "Quit"
+                if event.key == pygame.K_RETURN:
+                    if selected == "Start":
+                        game()
+                    elif selected == "Quit":
+                        pygame.quit()
+                        quit()
+        draw("Aim Trainer", screen, 90, 150, pygame.font.SysFont(Font, 112), color1)
+        draw("Start", screen, 90, 300, pygame.font.SysFont(Font, 100), color2)
         Clock.tick(50)
         time += 1
         if time % 100 == 0:
@@ -82,8 +83,9 @@ def main_menu():
         pygame.display.update()
 
 #Draw text on screen
-def draw(txt, surface, x, y, font = Font, color = Green):
-    txtRect = Font.render(txt, 1, color).getRect()
+def draw(txt, Font):
+    textSurface = Font.render(txt, True, Black)
+    return textSurface, textSurface.get_rect()
 
 #Code for different difficulties 
 def difficulty_lvl(difficulty):
@@ -92,7 +94,6 @@ def difficulty_lvl(difficulty):
 #Change sensitivity
 def sensitivity():
     pass
-
 
 #Game over tab
 def game_over(shots, hits, difficulty, score):
